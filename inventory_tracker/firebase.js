9cbdaf7c2c65d5902639e88fc904d7d62cad5b49
Amy Liu <al577@duke.edu>
2024-08-12 18:34:01 -0500

    adding base structure changes

---
+++ inventory_tracker/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCQMr-NGPanulfOzVbW-KCur_3Z3Wp3HU",
+  authDomain: "inventory-tracker-97d37.firebaseapp.com",
+  projectId: "inventory-tracker-97d37",
+  storageBucket: "inventory-tracker-97d37.appspot.com",
+  messagingSenderId: "762622863180",
+  appId: "1:762622863180:web:d7d178dcfd3f3db02fd1e9",
+  measurementId: "G-1CM30TC8C4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+export {firestore}
