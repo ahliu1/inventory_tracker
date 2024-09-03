dbb1e5b04ef4186c3df216bdf188b9aee80294a5
Amy <77812893+ahliu1@users.noreply.github.com>
2024-09-03 11:28:51 -0400

    Update firebase.js

--- inventory_tracker/firebase.js
+++ inventory_tracker/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
+import {key} from "./api_key"; // Please note that you'll need to get your own API key from Firebase and create an api_key file for this. 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
